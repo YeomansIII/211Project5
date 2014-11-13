b711b231ce655fa72cb5e0458f914549d1839e3a
Jason D Yeomans <jyeoman2@gmu.edu>
2014-11-13 18:28:43 -0500

    Add LLNode.java

---
+++ src/LLNode.java
@@ -0,0 +1,40 @@
+
+public class LLNode implements Node {
+
+	private Comparable data;
+	private Node next;
+	private Node prev;
+	
+	@Override
+	public Comparable getData() {
+		// TODO Auto-generated method stub
+		return data;
+	}
+
+	@Override
+	public Node add(Node thing) {
+		// TODO Auto-generated method stub
+		Node start;
+		
+		return null;
+	}
+
+	@Override
+	public Node search(Comparable item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNext(Node next) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPrev(Node prev) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
