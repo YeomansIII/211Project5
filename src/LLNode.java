1f1ae146eca2eea3f52b812e2feb3aa10ed8b1c3
Pedro Dejesus <pdejesu3@gmu.edu>
2014-11-13 23:15:23 -0500

    Test Cases Complete

--- src/LLNode.java
+++ src/LLNode.java
@@ -1,3 +1,16 @@
+//1
+//The sorted ArrayList is slower to add to than the Linked List because when you add elements to 
+//an ArrayList, because every time you insert an element, the array must re-adjust its size to accommodate newly entered elements.
+//Also, when trying to insert an element into a certain spot in the array, it must shift to fit it into its place.
+//A Linked List is better for adding elements because it does not require re-adjustment.
+//2
+//The add is so quick for the unsorted array because it does not need to place elements into a correct order when added.
+//3
+//Our numbers are not identical because the driver class uses the Random class to create random numbers for every run of Driver.
+//The speed of certain parts of the program is being measured in terms of time.
+//4
+
+//5
 
 public class LLNode implements Node {
 
