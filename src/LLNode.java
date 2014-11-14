38b9e147cd695806eeb6c219d64fdab6499b830d
Jason Yeomans <jyeo098@gmail.com>
2014-11-13 23:16:20 -0500

    Modified LLNode

--- src/LLNode.java
+++ src/LLNode.java
@@ -1,20 +1,19 @@
-
 public class LLNode implements Node {
 
 	@SuppressWarnings("rawtypes")
 	private Comparable data;
 	protected LLNode next;
 	protected LLNode prev;
-	
+
 	@SuppressWarnings("rawtypes")
 	public LLNode(Comparable c) {
 		data = c;
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public Comparable getData() {
-		
+
 		return data;
 	}
 
@@ -23,10 +22,11 @@ public class LLNode implements Node {
 	public Node add(Node thing) {
 		System.out.println(thing.getData().toString());
 		LLNode current = getStart();
-		//LLNode current = this;
-		while(current.next != null) {
-			if((current.getData().compareTo(thing.getData()) < 0) && (current.next.getData().compareTo(thing.getData()) > 0)) {
-				//System.out.println("Success Add IF STATEMENT");
+		// LLNode current = this;
+		while (current.next != null) {
+			if ((current.getData().compareTo(thing.getData()) < 0)
+					&& (current.next.getData().compareTo(thing.getData()) > 0)) {
+				// System.out.println("Success Add IF STATEMENT");
 				thing.setNext(current.next);
 				thing.setPrev(current);
 				current.next.setPrev(thing);
@@ -36,7 +36,7 @@ public class LLNode implements Node {
 				current = current.next;
 			}
 		}
-		if(current.next==null) {
+		if (current.next == null) {
 			current.setNext(thing);
 		}
 
@@ -46,11 +46,11 @@ public class LLNode implements Node {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public Node search(Comparable item) {
-		//LLNode current = getStart();
+		// LLNode current = getStart();
 		LLNode current = this;
-		while(current.next != null) {
+		while (current.next != null) {
 			System.out.println(current.getData().toString());
-			if(current.getData().compareTo(item)==0) {
+			if (current.getData().compareTo(item) == 0) {
 				return current;
 			}
 			current = current.next;
@@ -67,13 +67,39 @@ public class LLNode implements Node {
 	public void setPrev(Node prev) {
 		this.prev = (LLNode) prev;
 	}
-	
+
 	private LLNode getStart() {
 		LLNode current = this;
-		while(current.prev != null) {
+		while (current.prev != null) {
 			current = current.prev;
 		}
 		return current;
 	}
 
 }
+
+// 1
+// The sorted ArrayList is slower to add to than the Linked List because when
+// you add elements to an ArrayList, because every time you insert an element,
+// the array must re-adjust its size to accommodate newly entered elements.
+// Also, when trying to insert an element into a certain spot in the array, it
+// must shift to fit it into its place. A Linked List is better for adding
+// elements because it does not require re-adjustment.
+// 2
+// The add is so quick for the unsorted array because it does not need to place
+// elements into a correct order when added, just immediately add the element to
+// the end.
+// 3
+// Our numbers are not identical because the driver class uses the Random class
+// to create random numbers for every run of Driver. The speed of certain parts
+// of the program is being measured in terms of time.
+// 4
+// The person objects in Driver have a random ID and therefore are sorted when
+// they are added to the list, as none of the person objects are ever identical,
+// the placement of the person objects are not identical.
+// 5
+// There are many methods of searching for the desired Comparable. Currently we
+// go through the list from the beginning to the end, another method is to start
+// in the middle and check the comparable values of the next and previous nodes.
+// From there you can keep chopping the list in half until you are left with
+// your desired node.
